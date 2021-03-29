bb752131375ff49d89b24fd458b0cb35c15c4fc9
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-03-29 17:12:47 +0700

    Update looblike.cpp

--- looblike.cpp
+++ looblike.cpp
@@ -1,5 +1,19 @@
 #include<stdio.h>
 int main(){
-	
+	int num,a,chk;
+	scanf("%d",&num);
+	int data[num];
+	if(num>=2&&num<=1000)
+	{
+		for(a=0;a<num;a++)
+		{
+			scanf("%d ",&chk);
+			if(chk>=1&&chk<=10000)
+			{
+				data[a] = chk;
+			}
+			else return 0;
+		}
+	}
 	return 0;
 }
