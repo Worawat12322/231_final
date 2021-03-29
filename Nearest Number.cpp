29d7488500a50a6c975ff6384f6b7a65fac848f8
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-03-29 18:20:19 +0700

    update Near num

--- Nearest Number.cpp
+++ Nearest Number.cpp
@@ -1,11 +1,29 @@
 #include<stdio.h>
 #include<string>
 int main(){
-	int n,c;
+	int n,c,k;
+	int chk,mod,ans=0;
 	scanf("%d %d",&n,&c);
 	if((n>0&&n<32750)&&(n>0&&n<32750))
 	{
-		
+		for(k=n;;k++)
+		{
+			chk = k;
+			while(chk!=0)
+			{
+				mod = chk%10;
+				chk = chk/10;
+				if(mod == c)
+				{
+					ans = 1;
+				}
+			}
+			if(ans==1)
+			{
+				break;
+			}
+		}
+		printf("%d",k);
 	}
 	return 0;
 }
