f008871a74e1c8079a98b55230600c0210a52df3
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-03-29 17:29:53 +0700

    Update looblike.cpp

--- looblike.cpp
+++ looblike.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-	int num,a,b,chk=0,limit=0;
+	int num,a,b,c,d,chk=0,limit=0;
 	scanf("%d",&num);
 	int data[num];
 	int max,mx_lo[num]={0};
@@ -15,15 +15,26 @@ int main(){
 			}
 			else return 0;
 		}
-		max=data[0];
+		max=data[0]; mx_lo[0]=1 ;
 		for(b=1;b<num;b++)
 		{
 			if(max<data[b])
 			{
 				max=data[b];
-				mx_lo[0]=b+1;
+				mx_lo[0]=b;
 			}
-			
+		}
+		for(c=mx_lo[0];c<num;c++)
+		{
+			if(max==data[c])
+			{
+				limit++;
+				mx_lo[limit] = c;
+			}
+		}
+		for(d=0;d<=limit;d++)
+		{
+			printf("%d ",mx_lo[d]);
 		}
 	}
 	return 0;
