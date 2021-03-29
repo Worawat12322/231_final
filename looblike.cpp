a5f6b46e40fb8acbaa8538bd8bf0ff84d3fe5f5c
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-03-29 19:09:22 +0700

    fix looblike

--- looblike.cpp
+++ looblike.cpp
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
-	int num,a,b,c,d,chk=0,limit=0;
+	int num,a,b,c,d,e,chk=0,limit=0;
 	scanf("%d",&num);
 	int data[num];
-	int max,mx_lo[num]={0};
+	int max;
 	if(num>=2&&num<=1000)
 	{
 		for(a=0;a<num;a++)
@@ -15,26 +15,43 @@ int main(){
 			}
 			else return 0;
 		}
-		max=data[0]; mx_lo[0]=0 ;
+		max=data[0];
 		for(b=1;b<num;b++)
 		{
 			if(max<data[b])
 			{
 				max=data[b];
-				mx_lo[0]=b;
 			}
 		}
-		for(c=mx_lo[0]+1;c<num;c++)
+		int f,most[max+1]={0},max2,lo_max[max+1]={0};
+		for(e=0;e<num;e++)
 		{
-			if(max==data[c])
+			most[data[e]]++;
+		}
+		max2=most[1];
+		for(f=2;f<max+1;f++)
+		{
+			if(max2<most[f])
+			{
+				max2 = most[f];
+				lo_max[0] = f;
+			}
+		}
+		for(c=lo_max[0]+1;c<max+1;c++)
+		{
+			if(max2==most[c])
 			{
 				limit++;
-				mx_lo[limit] = c;
+				lo_max[limit]=c;
 			}
 		}
 		for(d=0;d<=limit;d++)
 		{
-			printf("%d ",mx_lo[d]+1);
+			if(d!=0)
+			{
+				printf(" ");
+			}
+			printf("%d",lo_max[d]);
 		}
 	}
 	return 0;
