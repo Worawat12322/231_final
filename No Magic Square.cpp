694f573d34329af6dc7c3a6bc9da2898db6c00eb
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-03-29 19:29:21 +0700

    Update No Magic Square.cpp

--- No Magic Square.cpp
+++ No Magic Square.cpp
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main(){
-	
+	int  a,b,c;
+	scanf("%d %d %d",&a,&b,&c);
+	int ar_a[a][a]={0},ar_b[b][b]={0},ar_c[c][c]={0};
+	int d,e,f,ans[2]={0};
+	for(d=0;d<a;d++)
+	{
+		scanf("")
+	}
 	return 0;
 }
